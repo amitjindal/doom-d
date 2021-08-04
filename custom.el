af21e1dad1289e3bf51eba141c6f24fda6c60328
Amit Jindal <amitjindal@aquevix.com>
2021-08-04 19:12:57 +0530

    Commit till here

---
+++ custom.el
@@ -0,0 +1,46 @@
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+   '(ansi-color-names-vector
+       ["#002b36" "#dc322f" "#859900" "#b58900" "#268bd2" "#d33682" "#2aa198" "#839496"])
+   '(custom-safe-themes
+       '("cbe59faa15a5f52bdf110b822509b1bbe7f080997c036d0ceb22675adc2e4d76" default))
+ '(fci-rule-color "#405A61")
+ '(jdee-db-active-breakpoint-face-colors (cons "#073642" "#268bd2"))
+ '(jdee-db-requested-breakpoint-face-colors (cons "#073642" "#859900"))
+ '(jdee-db-spec-breakpoint-face-colors (cons "#073642" "#56697A"))
+ '(objed-cursor-color "#dc322f")
+ '(pdf-view-midnight-colors (cons "#839496" "#002b36"))
+   '(rustic-ansi-faces
+       ["#002b36" "#dc322f" "#859900" "#b58900" "#268bd2" "#d33682" "#2aa198" "#839496"])
+ '(vc-annotate-background "#002b36")
+   '(vc-annotate-color-map
+       (list
+          (cons 20 "#859900")
+          (cons 40 "#959300")
+          (cons 60 "#a58e00")
+          (cons 80 "#b58900")
+          (cons 100 "#bc7407")
+          (cons 120 "#c35f0e")
+          (cons 140 "#cb4b16")
+          (cons 160 "#cd4439")
+          (cons 180 "#d03d5d")
+          (cons 200 "#d33682")
+          (cons 220 "#d63466")
+          (cons 240 "#d9334a")
+          (cons 260 "#dc322f")
+          (cons 280 "#ba3f41")
+          (cons 300 "#994d54")
+          (cons 320 "#775b67")
+          (cons 340 "#405A61")
+          (cons 360 "#405A61")))
+ '(vc-annotate-very-old-color nil))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
+(put 'projectile-ripgrep 'disabled nil)
